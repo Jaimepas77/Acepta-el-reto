e504610da4178b9e8e0b37cd9356f1cd434178e1
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2023-02-25 20:34:43 +0100

    Create 453_Eslabones.cpp

---
+++ 453_Eslabones.cpp
@@ -0,0 +1,53 @@
+// Usuario de Acepta el reto: jjjjjjjp022
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int e, r;
+	cin >> e;
+	while(e != 0) {
+		cin >> r;
+		vector<int> pred(e, -1);//Padre de cada especie
+		for(int i = 0; i < r; i++) {
+			int o, d;
+			cin >> o >> d;
+			pred[d-1] = o;
+		}
+	
+		int padre = -1;
+		// for(int i = 1; i <= e; i++) {
+			// if(pred[i-1] == -1) {
+				// padre = i;
+				// break;
+			// }
+		// }
+		
+		bool todas = true;
+		vector<bool> visto(e, false);
+		for(int i = 1; i <= e && todas; i++) {
+			int act = i;
+			while(act != padre && !visto[act-1]) {
+				visto[act-1] = true;
+				if (pred[act-1] != -1) {
+					act = pred[act-1];
+				}
+				else if(pred[act-1] == -1 && padre == -1) {
+					padre = act;
+				}
+				else {
+					todas = false;
+					break;
+				}
+			}
+		}
+		
+		if(todas)
+			cout << "TODAS\n";
+		else
+			cout << "FALTA ALGUNA\n";
+
+		cin >> e;
+	}
+}
